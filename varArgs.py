cd2be9dfd1483dfb8f7d07991701611ca5e95eeb
Şenol AKKAŞ <info@senolakkas.com>
2016-04-18 02:17:19 +0300

    Update varArgs.py

--- varArgs.py
+++ varArgs.py
@@ -1,4 +1,3 @@
-####Python
 def f(a, b=10, c=20, *args, **kargs):
     d = args[2] if len(args)>2 else 40
     # if key "x" exists in kargs dict returns its value (kargs["x"]), otherwise returns default value 40
@@ -9,3 +8,26 @@ print f(1,2)
 print f(1,2,3,4)
 print f(1,2,3,4,5,x="x")
 print f(1,2,3,4,5,6,7)
+
+'''
+-- javascript
+function f(k,inc) {    
+    var sum = 0;   
+    for (num in arguments) {
+        sum += num * k;
+        k += inc;
+    }  
+    return sum;
+}
+
+--- Java 
+    public static int f(int k,int inc, int... args) {
+        int sum = 0;        
+        for (int num : args) {
+            sum += num * k;
+            k += inc;
+        }  
+        return sum;
+    }
+
+'''
